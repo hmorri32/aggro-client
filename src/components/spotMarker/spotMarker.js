b42101686d227439af57a25cf04e9814f3e8b9fa
Hugh Morrison <hmorri32@gmail.com>
2018-01-07 13:52:22 -0700

    renders spot markers

---
+++ src/components/spotMarker/spotMarker.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import AppContainer from "../../containers/AppContainer";
+import { Link } from "react-router-dom";
+
+L.Icon.Default.imagePath = ".";
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
+class SpotMarker extends Component {
+  render() {
+    const { spot, resetMap, zoom, lat, lon } = this.props;
+    return <Marker position={[parseFloat(lat), parseFloat(lon)]} />;
+  }
+}
+
+export default SpotMarker;
