2b1407f0bb98fb1a75b294058c79084d7e38156f
Hugh Morrison <hmorri32@gmail.com>
2018-01-07 12:33:51 -0700

    configures redux store and router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/app/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, applyMiddleware } from "redux";
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+
+import { ConnectedRouter } from "react-router-redux";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import App from "./components/app/App";
+import { root } from "./reducers/rootReducer";
+
+import "./index.css";
+import "leaflet/dist/leaflet.css";
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(root,devTools,applyMiddleware(middleware, thunk, logger));
+
+const router = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Route exact path="/" component={App} />
+    </ConnectedRouter>
+  </Provider>
+);
+
+render(router, document.getElementById("root"));
+// registerServiceWorker();
