df3fa7703385693534730a51a38b17023ec7259c
Hugh Morrison <hmorri32@gmail.com>
2018-01-10 11:38:57 -0700

    init forecast component

---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Forecast extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className="login">sup</div>;
+  }
+}
+
+export default Forecast;
