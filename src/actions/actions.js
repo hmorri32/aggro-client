b1659528b5f4aa22a364530a51c0df4f56ad6ba9
Hugh Morrison <hmorri32@gmail.com>
2018-01-10 11:38:35 -0700

    lints actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -9,7 +9,7 @@ export const fetchSpots = () => {
   return dispatch => {
     // fetch("http://aggro-api.herokuapp.com/api/v1/spots")
     fetch("http://localhost:3000/api/v1/spots", {
-      headers: {Authorization: sessionStorage.jwt}
+      headers: { Authorization: sessionStorage.jwt }
     })
       .then(response => response.json())
       .then(spots => dispatch(addSpots(spots)));
