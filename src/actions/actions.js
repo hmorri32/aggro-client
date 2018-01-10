6c359f4fc309f2a837db6371f2675809f569a7d2
Hugh Morrison <hmorri32@gmail.com>
2018-01-10 10:27:10 -0700

    adds JWT to headers on fetch request

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -8,7 +8,9 @@ export const addSpots = spots => {
 export const fetchSpots = () => {
   return dispatch => {
     // fetch("http://aggro-api.herokuapp.com/api/v1/spots")
-    fetch("http://localhost:3000/api/v1/spots")
+    fetch("http://localhost:3000/api/v1/spots", {
+      headers: {Authorization: sessionStorage.jwt}
+    })
       .then(response => response.json())
       .then(spots => dispatch(addSpots(spots)));
   };
