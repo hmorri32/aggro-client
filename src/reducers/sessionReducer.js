d1b287f766bd26efa4db2f27bfe029c7cc561a9c
Hugh Morrison <hmorri32@gmail.com>
2018-01-09 17:52:20 -0700

    sessionreducer to handle jwt presence

--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -5,7 +5,8 @@ import initialState from "./initialState";
 export default function sessionReducer(state = initialState.session, action) {
   switch (action.type) {
   case types.LOG_IN_SUCCESS:
-    // browserHistory.push("/");
+    return !!sessionStorage.jwt;
+  case types.LOG_OUT:
     return !!sessionStorage.jwt;
   default:
     return state;
