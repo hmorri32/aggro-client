4ac0b0bafcdb88e57ca23d530d821b793a704a3b
Hugh Morrison <hmorri32@gmail.com>
2018-01-09 17:51:23 -0700

    adds logout method

--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -3,3 +3,8 @@ import * as types from "./actionTypes";
 export function loginSuccess() {
   return { type: types.LOG_IN_SUCCESS };
 }
+
+export function logOutUser() {
+  sessionStorage.removeItem("jwt");
+  return { type: types.LOG_OUT };
+}
