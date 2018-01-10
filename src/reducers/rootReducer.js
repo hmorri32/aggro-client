5e1359f0701554accfe02be368a28552e5be53dd
Hugh Morrison <hmorri32@gmail.com>
2018-01-09 17:05:18 -0700

    sessionreducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 import { spots } from "./reducers";
+import sessionReducer from "./sessionReducer";
 
 export const root = combineReducers({
   spots,
+  sessionReducer,
   router: routerReducer
 });
