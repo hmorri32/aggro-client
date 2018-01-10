0bb91e6887dcb5f79f3802235b1e171d266831d6
Hugh Morrison <hmorri32@gmail.com>
2018-01-09 17:04:05 -0700

    formats and adds login route

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import logo from '../../logo.svg';
-import AppContainer from '../../containers/AppContainer'
-import SpotMap from '../spotMap/SpotMap'
-import {NavBar} from '../navBar/Navbar'
-import Hype from '../hype/Hype'
-import Feed from '../feed/Feed'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import AppContainer from "../../containers/AppContainer";
+import SpotMap from "../spotMap/SpotMap";
+import { NavBar } from "../navBar/Navbar";
+import Hype from "../hype/Hype";
+import Feed from "../feed/Feed";
+import Login from "../login/Login";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
-    this.props.fetchSpots()
+    this.props.fetchSpots();
   }
 
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Route exact path='/map' component={SpotMap}/>
+        <Route exact path="/map" component={SpotMap} />
         <Route exact path="/hype" component={Hype} />
         <Route exact path="/feed" component={Feed} />
+        <Route exact path="/login" component={Login} />
       </div>
     );
   }
