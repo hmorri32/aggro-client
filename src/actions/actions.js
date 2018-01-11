97a425258e01e22b26e41c4b0eb158a12bf0d67f
Hugh Morrison <hmorri32@gmail.com>
2018-01-11 13:17:38 -0700

    add spots with forecast action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,6 +5,13 @@ export const addSpots = spots => {
   };
 };
 
+export const addSpotWithForecast = spots => {
+  return {
+    type: "ADD_SPOTS_WITH_FORECAST",
+    spots
+  }
+}
+
 export const fetchSpots = () => {
   return dispatch => {
     // fetch("http://aggro-api.herokuapp.com/api/v1/spots")
@@ -15,3 +22,15 @@ export const fetchSpots = () => {
       .then(spots => dispatch(addSpots(spots)));
   };
 };
+
+export const fetchSpotsWithForecast = () => {
+  return dispatch => {
+    fetch("http://localhost:3000/api/v1/spots_with_forecast")
+      .then(res => res.json())
+      .then(spots => dispatch(addSpotWithForecast(spots)))
+
+    // const res = await fetch('http://localhost:3000/api/v1/spots_with_forecast')
+    // const spots = await res.json()
+    // dispatch(addSpotWithForecast(spots))
+  }
+}
