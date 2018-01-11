e431c77ec3ea7ee050f04b2fd89f4dd77d3e3f26
Hugh Morrison <hmorri32@gmail.com>
2018-01-10 18:09:53 -0700

    daily report chart render

--- src/components/spotDetail/SpotDetail.js
+++ src/components/spotDetail/SpotDetail.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import AppContainer from "../../containers/AppContainer.js";
 import { yungKeys } from "../../helpers/keys.js";
+import RC2 from "react-chartjs2";
+
 import "./SpotDetail.css";
 
 class SpotDetail extends Component {
@@ -16,6 +18,81 @@ class SpotDetail extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
     this.fetchSurflineData();
+    this.fetchSurflineTides();
+    this.fetchDailyReport();
+  }
+
+  fetchDailyReport = async () => {
+    const { spot } = this.state;
+    let spitDaily, surflineDaily, mswDaily;
+    if (spot.surfline_id) {
+      const res = await fetch(
+        `https://aggro-api.herokuapp.com/api/v1/surfline/daily/${
+          spot.surfline_id
+        }/parsed`
+      );
+      const forecast = await res.json();
+      surflineDaily = forecast[`${spot.surfline_id}`];
+      this.setState({ surflineDaily: surflineDaily });
+    }
+  };
+
+  dailyForecastChart() {
+    const { surflineDaily } = this.state;
+    let surflineMin = [];
+    let surflineMax = [];
+    let dates = [];
+    for (let key in surflineDaily) {
+      dates.push(key);
+      surflineMin.push(surflineDaily[key]["min_height"]);
+      surflineMax.push(surflineDaily[key]["max_height"]);
+    }
+    console.log(surflineMin);
+
+    const data = {
+      labels: dates,
+      datasets: [
+        {
+          label: "Surf Min",
+          backgroundColor: "mediumseagreen",
+          borderColor: "#52B3D9",
+          borderWidth: 1,
+          hoverBackgroundColor: "#C5EFF7",
+          hoverBorderColor: "#52B3D9",
+          data: surflineMin
+        },
+        {
+          label: "Surf Max",
+          backgroundColor: "#52B3D9",
+          borderColor: "#52B3D9",
+          borderWidth: 1,
+          hoverBackgroundColor: "white",
+          hoverBorderColor: "#52B3D9",
+          data: surflineMax
+        }
+      ]
+    };
+
+    const options = {
+      scales: {
+        xAxes: [
+          {
+            stacked: true
+          }
+        ],
+        yAxes: [
+          {
+            stacked: true
+          }
+        ]
+      }
+    };
+
+    return (
+      <div className="frames">
+        <RC2 data={data} options={options} type="bar" />
+      </div>
+    );
   }
 
   fetchSurflineData = async () => {
@@ -27,6 +104,15 @@ class SpotDetail extends Component {
     );
     const forecast = await res.json();
     this.setState({ surflineForecast: forecast });
+  };
+
+  fetchSurflineTides = async () => {
+    const { spot } = this.state;
+    const res = await fetch(
+      `http://localhost:3000/api/v1/surfline/tides/${spot.surfline_id}`
+    );
+    const tides = await res.json();
+    this.setState({ surflineTides: tides });
   };
 
   createMarkup() {
@@ -43,28 +129,66 @@ class SpotDetail extends Component {
     );
   }
 
+  renderConditions() {
+    const { surflineForecast } = this.state;
+    if (surflineForecast) {
+      const { generalCondition } = surflineForecast.Analysis;
+      if (generalCondition[0]) {
+        return `Conditions: ${generalCondition[0]}`;
+      }
+    }
+  }
+
+  tideChart() {
+    const { surflineTides } = this.state;
+    let tides = surflineTides.dataPoints.map(d => d.height);
+    let times = surflineTides.dataPoints.map(t => t.utctime);
+    let halfTimes = times.splice(0, Math.floor(tides.length / 2));
+    let halfTides = tides.splice(0, Math.floor(tides.length / 2));
+
+    const data = {
+      labels: halfTimes,
+      datasets: [
+        {
+          label: "Tides",
+          fill: false,
+          borderColor: "#2C3E50",
+          pointBorderColor: "#2C3E50",
+          pointBackgroundColor: "#fff",
+          pointHoverBackgroundColor: "#2C3E50",
+          pointHoverBorderColor: "#fff",
+          pointRadius: 2,
+          pointHitRadius: 2,
+          data: halfTides
+        }
+      ]
+    };
+
+    return (
+      <div className="frames">
+        <RC2 data={data} type="line" />
+      </div>
+    );
+  }
+
   render() {
-    const { surflineForecast, spot } = this.state;
+    const { surflineForecast, spot, surflineTides } = this.state;
     return (
       <div className="detail-container">
         <h2>{surflineForecast && surflineForecast.name}</h2>
-        <h3>
-          Conditions:{" "}
-          {surflineForecast &&
-            surflineForecast.Analysis.generalCondition[0]}{" "}
-        </h3>
-        <h3>
-          {surflineForecast &&
-            surflineForecast.Analysis.surfRange[0]}
-        </h3>
-
+        <h3>{this.renderConditions()}</h3>
+        <h3>{surflineForecast && surflineForecast.Analysis.surfRange[0]}</h3>
         {this.myJankyComponent()}
+        {this.dailyForecastChart()}
+        {surflineTides && this.tideChart()}
         <div className="frames">
           <iframe
             className="frames"
             width="100%"
             height="650"
-            src={`https://embed.windytv.com/embed2.html?lat=${spot.lat}&lon=${spot.lon}&zoom=8&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C`}
+            src={`https://embed.windytv.com/embed2.html?lat=${spot.lat}&lon=${
+              spot.lon
+            }&zoom=8&level=surface&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0C`}
             frameBorder="0"
           />
           <iframe
@@ -72,7 +196,9 @@ class SpotDetail extends Component {
             width="100%"
             height="650"
             frameBorder="0"
-            src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}&q=${spot.lat}, ${spot.lon}&maptype=satellite`}
+            src={`https://www.google.com/maps/embed/v1/place?key=${yungKeys}&q=${
+              spot.lat
+            }, ${spot.lon}&maptype=satellite`}
           />
         </div>
       </div>
