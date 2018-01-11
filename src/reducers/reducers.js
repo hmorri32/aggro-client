b65324796d111789559f3ba53b3dd5269bf1628b
Hugh Morrison <hmorri32@gmail.com>
2018-01-11 13:17:57 -0700

    reducer spots with forecast

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -7,3 +7,13 @@ export const spots = (state = [], action) => {
     return state;
   }
 };
+
+export const spotsWithForecast = (state = [], action) => {
+  switch (action.type) {
+  case "ADD_SPOTS_WITH_FORECAST":
+    state = [];
+    return state.concat(action.spots);
+  default:
+    return state;
+  }
+};
